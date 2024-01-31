606b6ddaac641fd4c20c483cc5831f84fe2454db
Deepak Dubey <111698874+deepdubey01@users.noreply.github.com>
2024-01-31 18:17:28 +0530

    Service page created at EOD - 31-12

--- api.py
+++ api.py
@@ -1,7 +1,15 @@
-from datetime import datetime
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
-from src.services import fetch_data_from_table, add_student, update_student_status, delete_student_data
+from src.services import (
+    fetch_data_from_table,
+    add_student,
+    update_student_status,
+    delete_student_data,
+    add_teacher,
+    update_teacher_status,
+    delete_teacher_data,
+)
+from datetime import datetime
 
 app = FastAPI()
 
@@ -18,22 +26,52 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/student/")
+student_router = APIRouter(prefix="/student", tags=["Student"])
+teacher_router = APIRouter(prefix="/teacher", tags=["Teacher"])
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the API!"}
+
+@student_router.get("/")
 def get_students(id: int = None):
-    data = fetch_data_from_table(id)
+    data = fetch_data_from_table(id,'students')
     return {"data": data}
 
-@app.post("/student/")
+@student_router.post("/")
 def add_student_route(name: str, dob: str, status: str):
     message = add_student(name, dob, status)
     return message
 
-@app.put("/student/{student_id}")
-def update_status(student_id: int, status: str):
+@student_router.put("/{student_id}")
+def update_student(student_id: int, status: str):
     data = update_student_status(student_id, status)
     return {"data": data}
 
-@app.delete("/student/{student_id}")
+@student_router.delete("/{student_id}")
 def delete_student(student_id: int):
     data = delete_student_data(student_id)
     return {"data": data}
+
+@teacher_router.get("/")
+def get_teachers(id: int = None):
+    data = fetch_data_from_table(id,'teachers')
+    return {"data": data}
+
+@teacher_router.post("/")
+def add_teacher_route(name: str, dob: str, status: str):
+    message = add_teacher(name, dob, status)
+    return message
+
+@teacher_router.put("/{teacher_id}")
+def update_teacher(teacher_id: int, status: str):
+    data = update_teacher_status(teacher_id, status)
+    return {"data": data}
+
+@teacher_router.delete("/{teacher_id}")
+def delete_teacher(teacher_id: int):
+    data = delete_teacher_data(teacher_id)
+    return {"data": data}
+
+app.include_router(student_router)
+app.include_router(teacher_router)
--- src/services.py
+++ src/services.py
@@ -5,16 +5,17 @@ from dbConnection.database import DatabaseConnection
 
 db_instance = DatabaseConnection()
 
-def fetch_data_from_table(id=None):
+
+def fetch_data_from_table(id=None, table_name=str):
     conn = db_instance.get_connection()
     cursor = None
     try:
         cursor = conn.cursor()
         if id is None:
-            query = "SELECT * FROM students"
+            query = f"SELECT * FROM {table_name}"
             cursor.execute(query)
         else:
-            query = "SELECT * FROM students WHERE id = %s"
+            query = f"SELECT * FROM {table_name} WHERE id = %s"
             cursor.execute(query, (id,))
         rows = cursor.fetchall()
         columns = [col[0] for col in cursor.description]
@@ -27,10 +28,11 @@ def fetch_data_from_table(id=None):
         return result
     except Error as e:
         print("Error:", e)
-        raise 
+        raise
     finally:
         if cursor:
             cursor.close()
+
 
 def update_student_status(student_id: int, status: str):
     conn = db_instance.get_connection()
@@ -105,3 +107,98 @@ def parse_date(dob):
         except ValueError:
             pass
     raise ValueError("Invalid date format")
+
+
+
+
+
+def update_teacher_status(student_id: int, status: str):
+    conn = db_instance.get_connection()
+    cursor = None
+    try:
+        cursor = conn.cursor()
+        if status in ['Active', 'Inactive']:
+            query = "UPDATE teachers SET status = %s WHERE id = %s"
+            cursor.execute(query, (status, student_id))
+            conn.commit()
+            return {"message": f"Status updated to {status} successfully"}
+        else:
+            raise HTTPException(status_code=400, detail="Invalid status")
+    except Error as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if cursor:
+            cursor.close()
+
+def delete_teacher_data(student_id: int):
+    conn = db_instance.get_connection()
+    cursor = None
+    try:
+        cursor = conn.cursor()
+        query = "DELETE FROM teachers WHERE id = %s"
+        cursor.execute(query, (student_id,))
+        conn.commit()
+        return {"message": f"Student with ID {student_id} deleted successfully"}
+    except Error as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if cursor:
+            cursor.close()
+
+def add_teacher(name: str, dob: str, status: str):
+    conn = db_instance.get_connection()
+    cursor = None
+    try:
+        dob_formatted = parse_date(dob)
+        cursor = conn.cursor()
+        cursor.execute("SELECT id FROM teachers WHERE name = %s AND dob = %s", (name, dob_formatted))
+        existing_student = cursor.fetchone()
+        
+        if existing_student:
+            raise HTTPException(status_code=400, detail="Student with the same data already exists")
+        cursor.execute("SELECT rollno FROM students ORDER BY rollno DESC LIMIT 1")
+        last_roll = cursor.fetchone()
+        
+        if last_roll:
+            last_roll_number = last_roll[0]
+            next_roll_number = int(last_roll_number[1:]) + 1
+            new_rollno = f"S{next_roll_number:03}" 
+        else:
+            new_rollno = "S001"
+    
+        query = "INSERT INTO students (name, rollno, dob, status) VALUES (%s, %s, %s, %s)"
+        cursor.execute(query, (name, new_rollno, dob_formatted, status))
+        conn.commit()
+        return {"message": "Student added successfully"}
+    except Error as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if cursor:
+            cursor.close()
+
+# def fetch_teachers_from_table(id=None):
+    conn = db_instance.get_connection()
+    cursor = None
+    try:
+        cursor = conn.cursor()
+        if id is None:
+            query = "SELECT * FROM students"
+            cursor.execute(query)
+        else:
+            query = "SELECT * FROM students WHERE id = %s"
+            cursor.execute(query, (id,))
+        rows = cursor.fetchall()
+        columns = [col[0] for col in cursor.description]
+        result = []
+        for row in rows:
+            row_data = {}
+            for i in range(len(columns)):
+                row_data[columns[i]] = row[i]
+            result.append(row_data)
+        return result
+    except Error as e:
+        print("Error:", e)
+        raise 
+    finally:
+        if cursor:
+            cursor.close()
